cc2b5fcb83150bb885e93d8a552400bfb9e4e8d4
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-09-23 22:50:18 +0200

    dev(AbstractServiceEntityRepository.php): create base of abstract

---
+++ src/Repository/AbstractServiceEntityRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+abstract class AbstractServiceEntityRepository extends ServiceEntityRepository
+{
+    /**
+     * AbstractServiceEntityRepository constructor.
+     *
+     * @param ManagerRegistry $registry
+     */
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, $this->getTheEntityClassAttachedToTheRepositoryClass());
+    }
+
+    /**
+     * @return string
+     */
+    public function getTheEntityClassAttachedToTheRepositoryClass(): string
+    {
+        $repositoryClass = get_class($this);
+
+        return $this->convertRepositoryClassIntoEntityClass($repositoryClass);
+    }
+
+    /**
+     * @param string $repositoryClass
+     * @return string
+     */
+    public function convertRepositoryClassIntoEntityClass(string $repositoryClass): string
+    {
+        $entityClass = str_replace(
+            ['\\Repository\\', 'Repository'],
+            ['\\Entity\\', ''],
+            $repositoryClass
+        );
+
+        return $entityClass;
+    }
+
+    /**
+     * Used for functional tests
+     */
+    public function tearDown(): void
+    {
+        $this->_em->close();
+        $this->_em = null; // avoid memory leaks
+    }
+}

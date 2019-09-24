835907875eb54655fc52bea95335afcaebf4750e
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-09-24 14:02:38 +0200

    dev(AbstractRepositoryTest.php): setUp() > create base (WIP)

--- tests/Unit/AbstractRepositoryTest.php
+++ tests/Unit/AbstractRepositoryTest.php
@@ -2,7 +2,43 @@
 
 namespace App\Tests\Unit;
 
-class AbstractRepositoryTest
-{
+use App\Repository\AbstractRepository;
+use App\Util\RepositoryUtilInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Common\Persistence\Mapping\ClassMetadata;
+use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
 
+class AbstractRepositoryTest extends TestCase
+{
+    private $abstractRepository;
+
+    protected function setUp()
+    {
+        $repositoryUtil = $this->prophesize(RepositoryUtilInterface::class);
+        $repositoryUtil
+            ->convertRepositoryClassIntoEntityClass(Argument::any())
+            ->willReturn('__ENTITY_CLASS__');
+
+        $manager = $this->prophesize(EntityManagerInterface::class);
+
+        $registry = $this->prophesize(ManagerRegistry::class);
+        $registry
+            ->getManagerForClass(Argument::any())
+            ->willReturn($manager->reveal());
+
+        $this->abstractRepository = $this->getMockForAbstractClass(
+            AbstractRepository::class,
+            [
+                $registry->reveal(),
+                $repositoryUtil->reveal(),
+            ]
+        );
+    }
+
+    public function test_convertRepositoryClassIntoEntityClass()
+    {
+        $this->markTestIncomplete();
+    }
 }

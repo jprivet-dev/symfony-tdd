53aa772b4cd196a4265d74af1af6c8c7dffaa527
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-09-25 11:37:20 +0200

    chore(app): copy past elements from symfony-docker repo of Kevin Dunglas

---
+++ docker/app/docker-entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+set -e
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- php-fpm "$@"
+fi
+
+if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
+    PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-production"
+    if [ "$APP_ENV" != 'prod' ]; then
+        PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-development"
+    fi
+    ln -sf "$PHP_INI_RECOMMENDED" "$PHP_INI_DIR/php.ini"
+
+    # The first time volumes are mounted, the project needs to be recreated
+    if [ ! -f composer.json ]; then
+        composer create-project "symfony/skeleton $SYMFONY_VERSION" tmp --stability=$STABILITY --prefer-dist --no-progress --no-interaction
+        jq '.extra.symfony.docker=true' tmp/composer.json > tmp/composer.tmp.json
+        rm tmp/composer.json
+        mv tmp/composer.tmp.json tmp/composer.json
+
+        cp -Rp tmp/. .
+        rm -Rf tmp/
+    elif [ "$APP_ENV" != 'prod' ]; then
+        # Always try to reinstall deps when not in prod
+        composer install --prefer-dist --no-progress --no-suggest --no-interaction
+    fi
+
+	# Permissions hack because setfacl does not work on Mac and Windows
+	chown -R www-data var
+fi
+
+exec docker-php-entrypoint "$@"

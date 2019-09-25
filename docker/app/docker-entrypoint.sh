e68ce20ce967c36ddf134058b7b659cb3b1e9507
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-09-25 18:07:29 +0200

    dev(docker-entrypoint.sh): remove symfony/skeleton elements

--- docker/app/docker-entrypoint.sh
+++ docker/app/docker-entrypoint.sh
@@ -11,18 +11,10 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
     if [ "$APP_ENV" != 'prod' ]; then
         PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-development"
     fi
+
     ln -sf "$PHP_INI_RECOMMENDED" "$PHP_INI_DIR/php.ini"
 
-    # The first time volumes are mounted, the project needs to be recreated
-    if [ ! -f composer.json ]; then
-        composer create-project "symfony/skeleton $SYMFONY_VERSION" tmp --stability=$STABILITY --prefer-dist --no-progress --no-interaction
-        jq '.extra.symfony.docker=true' tmp/composer.json > tmp/composer.tmp.json
-        rm tmp/composer.json
-        mv tmp/composer.tmp.json tmp/composer.json
-
-        cp -Rp tmp/. .
-        rm -Rf tmp/
-    elif [ "$APP_ENV" != 'prod' ]; then
+    if [ "$APP_ENV" != 'prod' ]; then
         # Always try to reinstall deps when not in prod
         composer install --prefer-dist --no-progress --no-suggest --no-interaction
     fi

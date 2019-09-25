f9e1c378c2ed52f75e8d7acffadd99e33c32e4f7
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-09-25 08:03:10 +0200

    dev(ProducService.php): import & use ProductRepositoryInterface

--- src/Service/ProducService.php
+++ src/Service/ProducService.php
@@ -2,6 +2,19 @@
 
 namespace App\Service;
 
+use App\Repository\ProductRepositoryInterface;
+
 class ProducService implements ProducServiceInterface
 {
+    private $productRepository;
+
+    public function __construct(ProductRepositoryInterface $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
+    public function checkAll()
+    {
+        $products = $this->productRepository->findAll();
+    }
 }

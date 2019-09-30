275efcfea67b21db27d329e4d9b80323850a4ce8
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-09-30 16:42:52 +0200

    dev.refactor(AbstractRepository.php): move here convertRepositoryClassIntoEntityClass()

--- src/Repository/AbstractRepository.php
+++ src/Repository/AbstractRepository.php
@@ -3,36 +3,44 @@
 namespace App\Repository;
 
 use App\Exception\EntityDoesNotExistException;
-use App\Util\RepositoryUtilInterface;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
 
 abstract class AbstractRepository extends ServiceEntityRepository implements AbstractRepositoryInterface
 {
-    private $repositoryUtil;
-
     /**
      * {@inheritdoc}
      */
-    public function __construct(ManagerRegistry $registry, RepositoryUtilInterface $repositoryUtil)
+    public function __construct(ManagerRegistry $registry)
     {
-        $this->repositoryUtil = $repositoryUtil;
-        parent::__construct($registry, $this->getTheEntityClassAttachedToTheCurrentRepositoryClass());
+        $repositoryClass = get_class($this);
+        parent::__construct($registry, $this->getTheEntityClassAttachedToTheRepositoryClass($repositoryClass));
     }
 
     /**
      * {@inheritdoc}
      */
-    public function getTheEntityClassAttachedToTheCurrentRepositoryClass(): string
+    public function getTheEntityClassAttachedToTheRepositoryClass(string $repositoryClass): string
     {
-        $repositoryClass = get_class($this);
-        $entityClass = $this->repositoryUtil->convertRepositoryClassIntoEntityClass($repositoryClass);
+        $entityClass = $this->convertRepositoryClassIntoEntityClass($repositoryClass);
 
         if (class_exists($entityClass)) {
             return $entityClass;
         }
 
         throw new EntityDoesNotExistException($entityClass);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertRepositoryClassIntoEntityClass(string $repositoryClass): string
+    {
+        return str_replace(
+            ['\\Repository\\', 'Repository'],
+            ['\\Entity\\', ''],
+            $repositoryClass
+        );
     }
 
     /**

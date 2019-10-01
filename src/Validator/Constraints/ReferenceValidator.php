474b2ccf4dfad181e3194981d090b03bebe0b187
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-10-01 14:13:07 +0200

    dev(Reference.php, ReferenceValidator.php): create base of validator

---
+++ src/Validator/Constraints/ReferenceValidator.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+use Symfony\Component\Validator\Exception\UnexpectedValueException;
+
+class ReferenceValidator extends ConstraintValidator
+{
+    const PATTERN = '/^[a-zA-Z0-9]+$/';
+
+    public function validate($value, Constraint $constraint)
+    {
+        if (!$constraint instanceof Reference) {
+            throw new UnexpectedTypeException($constraint, Reference::class);
+        }
+
+        // custom constraints should ignore null and empty values to allow
+        // other constraints (NotBlank, NotNull, etc.) take care of that
+        if (null === $value || '' === $value) {
+            return;
+        }
+
+        if (!is_string($value)) {
+            // throw this exception if your validator cannot handle the passed type so that it can be marked as invalid
+            throw new UnexpectedValueException($value, 'string');
+
+            // separate multiple types using pipes
+            // throw new UnexpectedValueException($value, 'string|int');
+        }
+
+        if (!preg_match(self::PATTERN, $value, $matches)) {
+            $this->context->buildViolation($constraint->message)
+                ->setParameter('{{ string }}', $value)
+                ->addViolation();
+        }
+    }
+}

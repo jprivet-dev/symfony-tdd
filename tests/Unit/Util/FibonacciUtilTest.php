db0efc9d8ec29db8b10cf848c59d8dc56277e22a
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-10-01 12:44:30 +0200

    dev(FibonacciUtilTest.php): create base of test

---
+++ tests/Unit/Util/FibonacciUtilTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Tests\Unit\Util;
+
+use App\Util\FibonacciVersion01Util;
+use App\Util\FibonacciVersion02Util;
+use App\Util\FibonacciVersion03Util;
+use App\Util\FibonacciVersion04Util;
+use PHPUnit\Framework\TestCase;
+
+class FibonacciUtilTest extends TestCase
+{
+    private $fibonacciVersion01Util;
+    private $fibonacciVersion02Util;
+    private $fibonacciVersion03Util;
+    private $fibonacciVersion04Util;
+
+    protected function setUp()
+    {
+        $this->fibonacciVersion01Util = new FibonacciVersion01Util();
+        $this->fibonacciVersion02Util = new FibonacciVersion02Util();
+        $this->fibonacciVersion03Util = new FibonacciVersion03Util();
+        $this->fibonacciVersion04Util = new FibonacciVersion04Util();
+    }
+
+    /**
+     * @dataProvider termProvider
+     */
+    public function test_FibonacciVersion01Util(int $n, int $expectedTerm)
+    {
+        $this->assertSame($expectedTerm, $this->fibonacciVersion01Util->rank($n));
+        $this->assertSame($expectedTerm, $this->fibonacciVersion02Util->rank($n));
+        $this->assertSame($expectedTerm, $this->fibonacciVersion03Util->rank($n));
+        $this->assertSame($expectedTerm, $this->fibonacciVersion04Util->rank($n));
+    }
+
+    public function termProvider()
+    {
+        return [
+            [0, 0],
+            [1, 1],
+            [2, 1],
+            [3, 2],
+            [4, 3],
+            [5, 5],
+            [6, 8],
+            [7, 13],
+            [8, 21],
+            [9, 34],
+            [10, 55],
+            [11, 89],
+            [12, 144],
+        ];
+    }
+}

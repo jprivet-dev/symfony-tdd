7f69fc1d7b0d0f95149a0ae6de830d6b020826ad
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-10-04 22:11:53 +0200

    dev.debug(ProductController.php): test productService::validateReference()

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\ProductServiceInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,8 +11,11 @@ class ProductController extends AbstractController
     /**
      * @Route("/product", name="product")
      */
-    public function index()
+    public function index(ProductServiceInterface $productService)
     {
+        dump($productService->validateReference('abs123'));
+        dump($productService->validateReference('___'));
+
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/ProductController.php',

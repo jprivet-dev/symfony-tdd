d9514c70bfcbaedf0166b07d5a50621221f224d0
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-10-04 22:12:43 +0200

    dev(ProductServiceTest.php): create base of test

---
+++ tests/Service/ProductServiceTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Repository\ProductRepositoryInterface;
+use App\Service\ProductService;
+use App\Validator\Constraints\Reference;
+use Prophecy\Argument;
+use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class ProductServiceTest extends TestCase
+{
+    private $productRepository;
+    private $validator;
+    private $productService;
+
+    protected function setUp()
+    {
+        $this->productRepository = $this->prophesize(ProductRepositoryInterface::class);
+        $this->validator = $this->prophesize(ValidatorInterface::class);
+
+        $this->productService = new ProductService(
+            $this->productRepository->reveal(),
+            $this->validator->reveal()
+        );
+    }
+
+    public function testValidateReference()
+    {
+        $fakeReference = '__FAKE_REFERENCE__';
+
+        $this->validator
+            ->validate($fakeReference, Argument::type(Reference::class))
+            ->shouldBeCalledTimes(1);
+
+        $this->productService->validateReference($fakeReference);
+    }
+}

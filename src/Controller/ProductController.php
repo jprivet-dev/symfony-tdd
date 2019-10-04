e65a5555c81b2c2c75b1c8aa9d3a1a5d3ff9b0b3
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-10-04 14:57:41 +0200

    dev(ProductController.php): make new controller

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/product", name="product")
+     */
+    public function index()
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/ProductController.php',
+        ]);
+    }
+}

d456979434ba941f0c04fc972c4231f91a7126d1
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-10-04 22:12:10 +0200

    dev(ProductService.php): create base of validateReference()

--- src/Service/ProductService.php
+++ src/Service/ProductService.php
@@ -3,18 +3,32 @@
 namespace App\Service;
 
 use App\Repository\ProductRepositoryInterface;
+use App\Validator\Constraints\Reference;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ProductService implements ProductServiceInterface
 {
     private $productRepository;
+    private $validator;
 
-    public function __construct(ProductRepositoryInterface $productRepository)
+    public function __construct(
+        ProductRepositoryInterface $productRepository,
+        ValidatorInterface $validator
+    )
     {
         $this->productRepository = $productRepository;
+        $this->validator = $validator;
     }
 
     public function checkAll()
     {
         $products = $this->productRepository->findAll();
+    }
+
+    public function validateReference(string $reference): ?ConstraintViolationListInterface
+    {
+        $constraint = new Reference();
+        return $this->validator->validate($reference, $constraint);
     }
 }

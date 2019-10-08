09d0a40598eb60b8e6ea8e5e2fdfc1fcc61f0a41
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-10-08 13:32:05 +0200

    dev(HelloController.php): create base of controller

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController extends AbstractController
+{
+    /**
+     * @Route("/hello", name="hello")
+     */
+    public function index()
+    {
+        return $this->render('page/hello.html.twig', [
+            'controller_name' => 'HelloController',
+        ]);
+    }
+}

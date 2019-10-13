6e517664af094f6b3e09bfeb261ac6f261914b35
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-10-13 21:57:31 +0200

    dev(ConstraintValidatorTestCase.php): create app's ConstraintValidatorTestCase

---
+++ tests/ConstraintValidatorTestCase.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Component\Validator\Test\ConstraintValidatorTestCase as SymfonyConstraintValidatorTestCase;
+
+abstract class ConstraintValidatorTestCase extends SymfonyConstraintValidatorTestCase
+{
+}

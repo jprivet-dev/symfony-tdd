04b17c9fbdcb189c0881619343c9be8e71bc9d4b
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-10-15 01:07:27 +0200

    dev(AbstractClassExampleTest.php): create testPrintOutWithAnonymousClass() & testPrintOutWithDummyClass()

--- tests/Unit/Util/Example/AbstractClassExampleTest.php
+++ tests/Unit/Util/Example/AbstractClassExampleTest.php
@@ -7,15 +7,46 @@ use App\Util\Example\AbstractClassExample;
 
 class AbstractClassExampleTest extends TestCase
 {
+    /**
+     * @see https://phpunit.readthedocs.io/en/8.4/test-doubles.html#mocking-traits-and-abstract-classes
+     */
     public function testPrintOutWithMockForAbstractClassMethod()
     {
-        $abstract = $this->getMockForAbstractClass(AbstractClassExample::class);
+        $stub = $this->getMockForAbstractClass(AbstractClassExample::class);
 
-        $abstract
+        $stub
             ->expects($this->once())
             ->method('abstractMethod')
             ->willReturn('foo');
 
-        $this->assertSame('foo', $abstract->concreteMethod());
+        $this->assertSame('foo', $stub->concreteMethod());
+    }
+
+    /**
+     * @see https://mnapoli.fr/anonymous-classes-in-tests/
+     */
+    public function testPrintOutWithAnonymousClass()
+    {
+        $class = new class() extends AbstractClassExample {
+            protected function abstractMethod(): string
+            {
+                return 'foo';
+            }
+        };
+
+        $this->assertSame('foo', $class->concreteMethod());
+    }
+
+    public function testPrintOutWithDummyClass()
+    {
+        $dummy = new Dummy();
+        $this->assertSame('foo', $dummy->concreteMethod());
+    }
+}
+
+class Dummy extends AbstractClassExample {
+    protected function abstractMethod(): string
+    {
+        return 'foo';
     }
 }

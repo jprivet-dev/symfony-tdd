51286bd28de10f5690869cbfa362339ab2f960ba
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-10-21 22:24:24 +0200

    dev(SmokeTest.php): udpate method & use constants

--- tests/Functional/SmokeTest.php
+++ tests/Functional/SmokeTest.php
@@ -2,27 +2,33 @@
 
 namespace App\Tests\Functional;
 
+use App\Params\ParamsRoutes;
 use App\Tests\WebTestCase;
 
 class SmokeTest extends WebTestCase
 {
     /**
-     * @dataProvider urlProvider
+     * @dataProvider routeProvider
      * @param string $route
      */
-    public function testResponseIsSuccessful(string $route)
+    public function testPageIsSuccessful(string $route)
     {
         $client = self::createClient();
 
+        /*
+         * This method does not follow the following principles
+         * https://symfony.com/doc/current/best_practices.html#hardcode-urls-in-a-functional-test
+         * In our context, we have no constraints on public URLs.
+         */
         $url = $client->getContainer()->get('router')->generate($route);
         $client->request('GET', $url);
 
         $this->assertResponseIsSuccessful();
     }
 
-    public function urlProvider()
+    public function routeProvider()
     {
-        yield ['hello'];
-        yield ['product'];
+        yield [ParamsRoutes::HELLO];
+        yield [ParamsRoutes::PRODUCT];
     }
 }

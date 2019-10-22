a22b2aa7275473e5544f22b25789830ad201636b
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-10-22 13:28:39 +0200

    dev.generate(NewsController.php, index.html.twig): make controller News

---
+++ src/Controller/NewsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NewsController extends AbstractController
+{
+    /**
+     * @Route("/news", name="news")
+     */
+    public function index()
+    {
+        return $this->render('news/index.html.twig', [
+            'controller_name' => 'NewsController',
+        ]);
+    }
+}

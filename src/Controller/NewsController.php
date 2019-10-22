15188b3d56a301e772020222a1076434153e17ef
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-10-22 13:54:03 +0200

    dev(NewsController.php): create base of controller

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -2,18 +2,39 @@
 
 namespace App\Controller;
 
+use App\Repository\NewsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class NewsController extends AbstractController
 {
-    /**
-     * @Route("/news", name="news")
-     */
-    public function index()
+    private $newsRepository;
+
+    public function __construct(NewsRepository $newsRepository)
     {
-        return $this->render('news/index.html.twig', [
-            'controller_name' => 'NewsController',
+        $this->newsRepository = $newsRepository;
+    }
+
+    /**
+     * @Route("/", name="news_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('news/news-index.html.twig', [
+            'collection' => $this->newsRepository->findAll(),
         ]);
+    }
+
+    /**
+     * @Route("/news/{slug}", name="news_item")
+     */
+    public function item(string $slug): Response
+    {
+        if (null === $news = $this->newsRepository->findOneBySlug($slug)) {
+            throw $this->createNotFoundException();
+        }
+
+        return $this->render('news/news-item.html.twig', ['item' => $news]);
     }
 }

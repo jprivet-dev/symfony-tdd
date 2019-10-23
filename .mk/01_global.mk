232d8fc2f60fea7b0493480b2fad00fe04e49f9c
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-10-23 22:54:56 +0200

    dev(01_global.mk): udpate app & db vars

--- .mk/01_global.mk
+++ .mk/01_global.mk
@@ -1,10 +1,13 @@
+APP_SERVICE = app
+DB_SERVICE = db
+
 D = docker
 DC = docker-compose
 EXEC = $(DC) exec
-APP_NAME = app
-APP = $(EXEC) $(APP_NAME)
-APP_ROOT = $(EXEC) --user 0 $(APP_NAME)
+APP = $(EXEC) $(APP_SERVICE)
+APP_ROOT = $(EXEC) --user 0 $(APP_SERVICE)
 PHP = $(APP) php
+DB = $(EXEC) ${DB_SERVICE}
 
 FOLDER_BUILD = build
 FOLDER_SRC = src

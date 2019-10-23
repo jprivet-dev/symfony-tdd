c4b69ae23f50cca918bebd328f79c120a8361098
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-10-24 00:11:38 +0200

    dev(07_database.mk): udpate db.create command

--- .mk/07_database.mk
+++ .mk/07_database.mk
@@ -5,18 +5,22 @@
 
 # Variables
 
-DATABASE_USER=user
-DATABASE_PASSWORD=userpass
+DATABASE_USER=root
+DATABASE_PASSWORD=rootpass
+DATABASE_HOST=db
+DATABASE_PORT=3306
 DATABASE_NAME=symfony_tdd
 
 # Commands
 
 PHONY: db.bash
 db.bash: ## Database: bash access
-	docker-compose exec db bash -c "mysql -u ${DATABASE_USER} -p${DATABASE_PASSWORD} ${DATABASE_NAME}"
+	$(DB) bash -c "mysql -u ${DATABASE_USER} -p${DATABASE_PASSWORD} ${DATABASE_NAME}"
 
 PHONY: db.create
+# With new \Symfony\Component\Dotenv\Dotenv()
+#@$(PHP) -r 'echo "Wait database... "; set_time_limit(15); require __DIR__."/vendor/autoload.php"; (new \Symfony\Component\Dotenv\Dotenv())->load(__DIR__."/.env"); $$host = getenv("DATABASE_HOST"); $$port = getenv("DATABASE_PORT"); echo "(".$$host.":".$$port.")\n"; for(;;) { if(@fsockopen($$host, $$port)) { break; }}'
 db.create: ## Database: drop & create
-	@$(PHP) -r 'echo "Wait database... "; set_time_limit(15); require __DIR__."/vendor/autoload.php"; (new \Symfony\Component\Dotenv\Dotenv())->load(__DIR__."/.env"); $$host = getenv("DATABASE_HOST"); $$port = getenv("DATABASE_PORT"); echo "(".$$host.":".$$port.")\n"; for(;;) { if(@fsockopen($$host, $$port)) { break; }}'
+	@$(PHP) -r 'echo "Wait database... "; set_time_limit(15); $$host = "${DATABASE_HOST}"; $$port = "${DATABASE_PORT}"; echo "(".$$host.":".$$port.")\n"; for(;;) { if(@fsockopen($$host, $$port)) { break; }}'
 	$(SYMFONY) doctrine:database:drop --if-exists --force
 	$(SYMFONY) doctrine:database:create --if-not-exists

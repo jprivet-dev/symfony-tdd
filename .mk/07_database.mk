904941372b6bdb9b6808cc4c9dd883189e8dc57d
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-10-24 14:07:28 +0200

    dev(07_database.mk): udpate commands

--- .mk/07_database.mk
+++ .mk/07_database.mk
@@ -16,13 +16,13 @@ DATABASE_DRIVER=pdo_mysql
 # Commands
 
 PHONY: db.bash
-db.bash: ## Database: bash access
+db.bash: ## Database: Bash access (mysql> ...)
 	$(DB) bash -c "mysql -u ${DATABASE_USER} -p${DATABASE_PASSWORD} ${DATABASE_NAME}"
 
 PHONY: db.create
 # With new \Symfony\Component\Dotenv\Dotenv()
 #@$(PHP) -r 'echo "Wait database... "; set_time_limit(15); require __DIR__."/vendor/autoload.php"; (new \Symfony\Component\Dotenv\Dotenv())->load(__DIR__."/.env"); $$host = getenv("DATABASE_HOST"); $$port = getenv("DATABASE_PORT"); echo "(".$$host.":".$$port.")\n"; for(;;) { if(@fsockopen($$host, $$port)) { break; }}'
-db.create: ## Database: drop & create
+db.create: ## Database: Drop & create
 	@$(PHP) -r 'echo "Wait database... "; set_time_limit(15); $$host = "${DATABASE_HOST}"; $$port = "${DATABASE_PORT}"; echo "(".$$host.":".$$port.")\n"; for(;;) { if(@fsockopen($$host, $$port)) { break; }}'
 	$(SYMFONY) doctrine:database:drop --if-exists --force
 	$(SYMFONY) doctrine:database:create --if-not-exists

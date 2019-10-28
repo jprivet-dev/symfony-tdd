66b36246e1fac883058674211e66aae12ce38c7b
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-10-28 23:52:11 +0100

    dev(CommentSystem.vue, index.js): create base of files

---
+++ assets/comments/CommentSystem.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <ol reversed v-if="comments.length">
+            <li v-for="comment in comments" :key="comment['@id']">{{ comment.body }}</li>
+        </ol>
+        <p v-else>No comments yet 🙁</p>
+
+        <form id="post-comment" @submit.prevent="onSubmit">
+            <textarea name="new-comment" v-model="newComment"
+                      placeholder="Your opinion matters! Send us your comment."></textarea>
+
+            <input type="submit" :disabled="!newComment">
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            news: {type: String, required: true}
+        },
+        methods: {
+            fetchComments() {
+                fetch(`/api/comments?news=${encodeURIComponent(this.news)}`)
+                    .then((response) => response.json())
+                    .then((data) => this.comments = data['hydra:member'])
+                ;
+            },
+            onSubmit() {
+                fetch('/api/comments', {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/ld+json',
+                        'Content-Type': 'application/ld+json'
+                    },
+                    body: JSON.stringify({news: this.news, body: this.newComment})
+                })
+                    .then(({ok, statusText}) => {
+                        if (!ok) {
+                            alert(statusText);
+                            return;
+                        }
+
+                        this.newComment = '';
+                        this.fetchComments();
+                    })
+                ;
+            }
+        },
+        data() {
+            return {
+                comments: [],
+                newComment: '',
+            };
+        },
+        created() {
+            this.fetchComments();
+        }
+    }
+</script>
---
+++ assets/comments/index.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import CommentSystem from './CommentSystem';
+
+new Vue({
+    el: '#comments',
+    components: {CommentSystem}
+});

a6c538c42a13bad3e6987fe3e4800df093e7e3fc
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-10-28 13:32:03 +0100

    dev(00_help.mk): insert space before title

--- .mk/00_help.mk
+++ .mk/00_help.mk
@@ -8,8 +8,9 @@ help:
 			split($$1, command, ":"); \
 			target=command[2]; \
 			description=$$2; \
+			space=""; \
 			if (target && target!="##")\
 				printf "\033[32m %-30s \033[0m%s\n", target, description; \
 			else \
-				printf "\033[33m%s\n", description; \
+				printf "\033[33m%s\n%s\n", space, description; \
 		}'

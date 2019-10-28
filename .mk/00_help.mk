fbbba77ffdaf2c519cb8b03f3f1eca7f266f7483
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-10-28 14:07:08 +0100

    dev(00_help.mk): update regex

--- .mk/00_help.mk
+++ .mk/00_help.mk
@@ -2,15 +2,22 @@
 
 .PHONY: help
 help:
-	@grep -E '(^[a-zA-Z_-.]+[^:]+:.*##[^#].*?$$)|(^##[^#])' $(MAKEFILE_LIST) \
+	@grep -E '(^[a-zA-Z_-.]+[^:]+:.*##.*?$$)|(^#{2})' $(MAKEFILE_LIST) \
 	| awk 'BEGIN {FS = "## "}; \
 		{ \
 			split($$1, command, ":"); \
 			target=command[2]; \
 			description=$$2; \
-			space=""; \
-			if (target && target!="##")\
+			# --- space --- \
+			if (target=="##") \
+				printf "\033[33m%s\n", ""; \
+			# --- title --- \
+			else if (target=="" && description!="") \
+				printf "\033[33m\n%s\n", description; \
+			# --- command + description --- \
+			else if (target!="" && description!="")\
 				printf "\033[32m %-30s \033[0m%s\n", target, description; \
+			# --- print nothing --- \
 			else \
-				printf "\033[33m%s\n%s\n", space, description; \
+				; \
 		}'

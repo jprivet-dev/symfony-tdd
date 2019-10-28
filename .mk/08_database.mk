33d30f7fa9bcd9efb20d2495256c6608c6ab1881
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-10-28 22:40:35 +0100

    dev(08_database.mk): adjust db.wait command

--- .mk/08_database.mk
+++ .mk/08_database.mk
@@ -4,7 +4,7 @@ PHONY: db.wait
 # With new \Symfony\Component\Dotenv\Dotenv()
 #@$(PHP) -r 'echo "Wait database... "; set_time_limit(15); require __DIR__."/vendor/autoload.php"; (new \Symfony\Component\Dotenv\Dotenv())->load(__DIR__."/.env"); $$host = getenv("DATABASE_HOST"); $$port = getenv("DATABASE_PORT"); echo "(".$$host.":".$$port.")\n"; for(;;) { if(@fsockopen($$host, $$port)) { break; }}'
 db.wait: ## Database: Wait database...
-	@$(PHP) -r 'echo "Wait database... "; set_time_limit(15); $$host = "${DATABASE_HOST}"; $$port = "${DATABASE_PORT}"; echo "(".$$host.":".$$port.")"; for(;;) { if(@fsockopen($$host, $$port)) { break; }}; echo "Database ready !")\n"'
+	@$(PHP) -r 'echo "Wait database $(DATABASE_HOST):$(DATABASE_PORT)...\n"; set_time_limit(15); for(;;) { if(@fsockopen($(DATABASE_HOST), $(DATABASE_PORT))) { break; }};'
 
 PHONY: db.bash
 db.bash: ## Database: Bash access (mysql> ...)

0d4d81dd64ed7ff2f904c82b437a9191245c0e3d
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-10-29 13:59:11 +0100

    dev(04_yarn.mk): update all

--- .mk/04_yarn.mk
+++ .mk/04_yarn.mk
@@ -1,13 +1,17 @@
 ## YARN
 
+.PHONY: yarn.install
+yarn.install: package.json yarn.lock ## Yarn: install
+	$(YARN) install
+
 .PHONY: yarn.encore.compile
-yarn.encore.compile: ## Yarn & Encore: Compile assets once.
+yarn.encore.compile: ## Webpack Encore: Compile assets once.
 	$(YARN) encore dev
 
 .PHONY: yarn.encore.watch
-yarn.encore.watch: ## Yarn & Encore: Recompile assets automatically when files change.
-	$(YARN) encore dev --watch
+yarn.encore.watch: ## Webpack Encore: Recompile assets automatically when files change.
+	$(YARN) encore dev --watch --watch-poll=300
 
 .PHONY: yarn.encore.deploy
-yarn.encore.deploy: ## Yarn & Encore: On deploy, create a production build.
+yarn.encore.deploy: ## Webpack Encore: On deploy, create a production build.
 	$(YARN) encore production

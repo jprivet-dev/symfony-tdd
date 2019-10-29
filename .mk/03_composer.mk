1de752ae1ca08680ff65d0db1575fd61c77a6d72
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-10-29 17:47:05 +0100

    dev(03_composer.mk): update composer.install.changes command

--- .mk/03_composer.mk
+++ .mk/03_composer.mk
@@ -9,7 +9,7 @@ composer.install.prod: ## Composer: Idem `composer.install` without dev elements
 	$(COMPOSER) install --verbose --no-progress --no-interaction --prefer-dist --optimize-autoloader --no-dev
 
 .PHONY: composer.install.changes
-composer.install.changes: vendor composer.lock ## Composer: Install only if there have been changes.
+composer.install.changes: vendor ## Composer: Install only if there have been changes.
 
 .PHONY: composer.update
 # --lock: only update the lock file hash to suppress warning about the lock file being out of date
@@ -24,8 +24,8 @@ composer.licenses: ## Composer: List the name, version and license of every pack
 	$(COMPOSER) licenses
 
 # This snippet will build the vendor directory, running composer install, only if the vendor directory does not exist.
-# Or if composer.lock file has changed since the last time you built the vendor directory.
-vendor: composer.lock
+# Or if composer.lock & composer.lock file has changed since the last time you built the vendor directory.
+vendor: composer.json composer.lock
 	$(MAKE) composer.install
 
 # This snippet will build the composer.lock file, running composer update, only if the composer.lock file does not exist.

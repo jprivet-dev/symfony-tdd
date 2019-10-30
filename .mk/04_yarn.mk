df513794ce0d6e7e3ad3876828fe106f7c1f1c71
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-10-30 10:12:47 +0100

    dev(04_yarn.mk): insert changes concept

--- .mk/04_yarn.mk
+++ .mk/04_yarn.mk
@@ -1,8 +1,27 @@
 ## YARN
 
+# This snippet will build the node_modules directory, running yarn install, only if the node_modules directory does not exist.
+# Or if yarn.lock & yarn.lock file has changed since the last time you built the node_modules directory.
+node_modules: package.json yarn.lock
+	@echo "\033[1;42mYarn : changes identified > triggered installation\033[0m"
+	$(MAKE) yarn.install
+
+# This snippet will build the yarn.lock file, running yarn upgrade, only if the yarn.lock file does not exist.
+# Or if package.json file has changed since the last time you built the yarn.lock file.
+yarn.lock: package.json
+	@echo "\033[1;42mYarn : changes identified > triggered upgrade\033[0m"
+	$(MAKE) yarn.upgrade
+
 .PHONY: yarn.install
-yarn.install: package.json yarn.lock ## Yarn: Install.
+yarn.install: ## Yarn: Install all dependencies.
 	$(YARN) install
+
+.PHONY: yarn.install.changes
+yarn.install.changes: node_modules ## Yarn: Install all dependencies (only if there have been changes).
+
+.PHONY: yarn.upgrade
+yarn.upgrade: ## Yarn: Upgrade packages to their latest version based on the specified range.
+	$(YARN) upgrade
 
 .PHONY: yarn.encore.compile
 yarn.encore.compile: ## Webpack Encore: Compile assets once.

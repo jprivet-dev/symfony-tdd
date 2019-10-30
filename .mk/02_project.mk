fdb4ba4343f1c98691a8265172853095b76cfa31
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-10-30 11:53:02 +0100

    dev(02_project.mk): check > udpate comment

--- .mk/02_project.mk
+++ .mk/02_project.mk
@@ -15,7 +15,7 @@ install: composer.install yarn.install ## Project: Install all (only if there ha
 ##
 
 .PHONY: check
-check: install symfony.security.check db.validate tests ## Launch of install, security, db-validate & tests
+check: install symfony.security.check db.validate tests ## Project: Launch of install, security, db validate & tests
 
 .PHONY: tests
 tests: phpunit ## Project: Launch all tests.

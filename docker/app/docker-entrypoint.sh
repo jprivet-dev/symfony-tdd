ba34cf6ba992408e3e74d65c283a4c66ec931106
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-10-31 23:31:07 +0100

    dev(docker-entrypoint.sh): disable prod part

--- docker/app/docker-entrypoint.sh
+++ docker/app/docker-entrypoint.sh
@@ -14,10 +14,10 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
 
     ln -sf "$PHP_INI_RECOMMENDED" "$PHP_INI_DIR/php.ini"
 
-    if [ "$APP_ENV" != 'prod' ]; then
-        # Always try to reinstall deps when not in prod
-        composer install --prefer-dist --no-progress --no-suggest --no-interaction
-    fi
+#    if [ "$APP_ENV" != 'prod' ]; then
+#        #Always try to reinstall deps when not in prod
+#        composer install --prefer-dist --no-progress --no-suggest --no-interaction
+#    fi
 
 	# Permissions hack because setfacl does not work on Mac and Windows
 	chown -R www-data var

8ec9c389e296f9f2eb4e6beb66b7c3f260fffd82
jprivet <jprivet@clever-age.com>
2019-11-12 13:31:20 +0100

    dev.refactor(AbstractClass): file renaming

--- src/Util/Example/AbstractClass.php
+++ src/Util/Example/AbstractClass.php
@@ -5,7 +5,7 @@ namespace App\Util\Example;
 /**
  * @see https://www.php.net/manual/en/language.oop5.abstract.php
  */
-abstract class AbstractClassExample
+abstract class AbstractClass
 {
     // Force Extending class to define this method
     abstract protected function abstractMethod(): string;

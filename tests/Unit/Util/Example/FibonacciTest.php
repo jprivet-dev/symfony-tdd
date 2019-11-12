bf949643ae53010b1ac013035d1fd9f4fc42876b
jprivet <jprivet@clever-age.com>
2019-11-12 13:56:11 +0100

    dev.refactor(FibonacciTest.php): use array instead

--- tests/Unit/Util/Example/FibonacciTest.php
+++ tests/Unit/Util/Example/FibonacciTest.php
@@ -10,45 +10,44 @@ use App\Util\Example\Fibonacci04;
 
 class FibonacciTest extends TestCase
 {
-    private $fibonacci01;
-    private $fibonacci02;
-    private $fibonacci03;
-    private $fibonacci04;
+    private $fibonacciList = [];
 
     protected function setUp(): void
     {
-        $this->fibonacci01 = new Fibonacci01();
-        $this->fibonacci02 = new Fibonacci02();
-        $this->fibonacci03 = new Fibonacci03();
-        $this->fibonacci04 = new Fibonacci04();
+        $this->fibonacciList = [
+            new Fibonacci01(),
+            new Fibonacci02(),
+            new Fibonacci03(),
+            new Fibonacci04(),
+        ];
     }
 
     /**
      * @dataProvider termProvider
      */
-    public function testCheckingAllVersions(int $n, int $expectedTerm)
+    public function testChecksAllVersionsWithAllTerms(int $n, int $expectedTerm)
     {
-        $this->assertSame($expectedTerm, $this->fibonacci01->rank($n));
-        $this->assertSame($expectedTerm, $this->fibonacci02->rank($n));
-        $this->assertSame($expectedTerm, $this->fibonacci03->rank($n));
-        $this->assertSame($expectedTerm, $this->fibonacci04->rank($n));
+        foreach ($this->fibonacciList as $fibonacci) {
+            $message = sprintf('Failed asserting with the class "%s" and the term "%s".', get_class($fibonacci), $n);
+            $this->assertSame($expectedTerm, $fibonacci->rank($n), $message);
+        }
     }
 
     public function termProvider()
     {
-            yield [0, 0];
-            yield [1, 1];
-            yield [2, 1];
-            yield [3, 2];
-            yield [4, 3];
-            yield [5, 5];
-            yield [6, 8];
-            yield [7, 13];
-            yield [8, 21];
-            yield [9, 34];
-            yield [10, 55];
-            yield [11, 89];
-            yield [12, 144];
-            //yield [20, 6765]; // Can be used to detect an error with Fibonacci04
+        yield [0, 0];
+        yield [1, 1];
+        yield [2, 1];
+        yield [3, 2];
+        yield [4, 3];
+        yield [5, 5];
+        yield [6, 8];
+        yield [7, 13];
+        yield [8, 21];
+        yield [9, 34];
+        yield [10, 55];
+        yield [11, 89];
+        yield [12, 144];
+        //yield [20, 6765]; // Can be used to detect an error with Fibonacci04
     }
 }

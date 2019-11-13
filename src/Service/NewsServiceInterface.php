dc5249e0b0f1652397ea6d54b8efd913fe98823b
jprivet <jprivet@clever-age.com>
2019-11-13 22:49:56 +0100

    dev(NewsServiceInterface.php): create __construct()

--- src/Service/NewsServiceInterface.php
+++ src/Service/NewsServiceInterface.php
@@ -2,6 +2,13 @@
 
 namespace App\Service;
 
+use App\Repository\NewsRepositoryInterface;
+
 interface NewsServiceInterface
 {
+    /**
+     * NewsServiceInterface constructor.
+     * @param NewsRepositoryInterface $newsRepository
+     */
+    public function __construct(NewsRepositoryInterface $newsRepository);
 }

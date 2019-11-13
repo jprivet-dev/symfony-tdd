6f970e5935468b7ed6d6ab8c62b74eedf6b47f64
jprivet <jprivet@clever-age.com>
2019-11-13 21:15:26 +0100

    dev(NewsControllerTest.php): create testComments()

--- tests/Functional/NewsControllerTest.php
+++ tests/Functional/NewsControllerTest.php
@@ -19,4 +19,19 @@ class NewsControllerTest extends WebTestCase
 
         $this->assertSame('Join us at SymfonyLive USA 2018!', $crawler->filter('h1')->text());
     }
+
+    public function testComments()
+    {
+        $client = static::createPantherClient();
+        $crawler = $client->request('GET', '/news/symfony-live-usa-2018');
+
+        $client->waitFor('#post-comment'); // Wait for the form to appear, it may take some time because it's done in JS
+        $form = $crawler->filter('#post-comment')->form(['new-comment' => 'Symfony is so cool!']);
+        $client->submit($form);
+
+        $client->waitFor('#comments ol'); // Wait for the comments to appear
+
+        $this->assertSame(self::$baseUri.'/news/symfony-live-usa-2018', $client->getCurrentURL()); // Assert we're still on the same page
+        $this->assertSame('Symfony is so cool!', $crawler->filter('#comments ol li:first-child')->text());
+    }
 }

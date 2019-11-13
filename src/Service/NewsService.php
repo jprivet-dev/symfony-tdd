124bf9e21ed387958ea54d2b98d7620411342c47
jprivet <jprivet@clever-age.com>
2019-11-13 22:49:50 +0100

    dev(NewsService.php): create __construct()

--- src/Service/NewsService.php
+++ src/Service/NewsService.php
@@ -2,6 +2,14 @@
 
 namespace App\Service;
 
+use App\Repository\NewsRepositoryInterface;
+
 class NewsService implements NewsServiceInterface
 {
+    private $newsRepository;
+
+    public function __construct(NewsRepositoryInterface $newsRepository)
+    {
+        $this->newsRepository = $newsRepository;
+    }
 }

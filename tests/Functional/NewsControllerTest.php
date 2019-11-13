c168b1bdf4c56d2a0ec8f2e2c0f1fcee6edc74e9
jprivet <jprivet@clever-age.com>
2019-11-13 22:19:00 +0100

    dev.refactor(NewsControllerTest.php): create & use constants

--- tests/Functional/NewsControllerTest.php
+++ tests/Functional/NewsControllerTest.php
@@ -6,32 +6,45 @@ use App\Tests\WebTestCase;
 
 class NewsControllerTest extends WebTestCase
 {
+    const NEWS_URL = '/news';
+    const NEWS_WEEK_601_SLUG = 'week-601';
+    const NEWS_SYMFONY_LIVE_SLUG = 'symfony-live-usa-2018';
+    const NEWS_SYMFONY_LIVE_URL = self::NEWS_URL . '/' . self::NEWS_SYMFONY_LIVE_SLUG;
+    const NEWS_SYMFONY_LIVE_TITLE = 'Join us at SymfonyLive USA 2018!';
+    const NEWS_COUNT = 2;
+
+    const COMMENTS_LIST_SELECTOR = '#comments-list';
+    const NEW_COMMENT_FORM_SELECTOR = '#new-comment-form';
+    const NEW_COMMENT_TEXTAREA_NAME = 'new-comment';
+    const NEW_COMMENT_TITLE = 'Symfony is so cool!';
+
     public function testNews()
     {
         $client = static::createPantherClient();
-        $crawler = $client->request('GET', '/news');
+        $crawler = $client->request('GET', self::NEWS_URL);
 
-        $this->assertCount(2, $crawler->filter('h1'));
-        $this->assertSame(['week-601', 'symfony-live-usa-2018'], $crawler->filter('article')->extract('id'));
+        $this->assertCount(self::NEWS_COUNT, $crawler->filter('h1'));
+        $this->assertSame([self::NEWS_WEEK_601_SLUG, self::NEWS_SYMFONY_LIVE_SLUG], $crawler->filter('article')->extract('id'));
 
-        $link = $crawler->selectLink('Join us at SymfonyLive USA 2018!')->link();
+        $link = $crawler->selectLink(self::NEWS_SYMFONY_LIVE_TITLE)->link();
         $crawler = $client->click($link);
 
-        $this->assertSame('Join us at SymfonyLive USA 2018!', $crawler->filter('h1')->text());
+        $this->assertSame(self::NEWS_SYMFONY_LIVE_TITLE, $crawler->filter('h1')->text());
     }
 
     public function testComments()
     {
         $client = static::createPantherClient();
-        $crawler = $client->request('GET', '/news/symfony-live-usa-2018');
+        $crawler = $client->request('GET', self::NEWS_SYMFONY_LIVE_URL);
 
-        $client->waitFor('#post-comment'); // Wait for the form to appear, it may take some time because it's done in JS
-        $form = $crawler->filter('#post-comment')->form(['new-comment' => 'Symfony is so cool!']);
+        $client->waitFor(self::NEW_COMMENT_FORM_SELECTOR); // Wait for the form to appear, it may take some time because it's done in JS
+
+        $form = $crawler->filter(self::NEW_COMMENT_FORM_SELECTOR)->form([self::NEW_COMMENT_TEXTAREA_NAME => self::NEW_COMMENT_TITLE]);
         $client->submit($form);
 
-        $client->waitFor('#comments ol'); // Wait for the comments to appear
+        $client->waitFor(self::COMMENTS_LIST_SELECTOR); // Wait for the comments to appear
 
-        $this->assertSame(self::$baseUri.'/news/symfony-live-usa-2018', $client->getCurrentURL()); // Assert we're still on the same page
-        $this->assertSame('Symfony is so cool!', $crawler->filter('#comments ol li:first-child')->text());
+        $this->assertSame(self::$baseUri . self::NEWS_SYMFONY_LIVE_URL, $client->getCurrentURL()); // Assert we're still on the same page
+        $this->assertSame(self::NEW_COMMENT_TITLE, $crawler->filter('#comments ol li:first-child')->text());
     }
 }

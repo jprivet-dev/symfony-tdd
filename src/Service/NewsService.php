ed0c5de0f35970cc37fa4d3a0eb743d98c5b90de
jprivet <jprivet@clever-age.com>
2019-11-13 23:04:06 +0100

    dev(NewsService.php): create collection() & item()

--- src/Service/NewsService.php
+++ src/Service/NewsService.php
@@ -2,14 +2,34 @@
 
 namespace App\Service;
 
+use App\Entity\News;
 use App\Repository\NewsRepositoryInterface;
 
 class NewsService implements NewsServiceInterface
 {
     private $newsRepository;
 
+    /**
+     * {@inheritDoc}
+     */
     public function __construct(NewsRepositoryInterface $newsRepository)
     {
         $this->newsRepository = $newsRepository;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function collection(): array
+    {
+        return $this->newsRepository->findAll();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function item(string $slug): News
+    {
+        return $this->newsRepository->findOneBySlug($slug);
     }
 }

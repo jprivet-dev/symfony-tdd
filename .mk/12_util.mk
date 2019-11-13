4858b453af632172ad107888879b1ed93156eecf
jprivet <jprivet@clever-age.com>
2019-11-13 13:57:18 +0100

    dev(12_util.mk): create util.php.strict command

--- .mk/12_util.mk
+++ .mk/12_util.mk
@@ -1,5 +1,10 @@
 ## UTIL
 
 .PHONY: util.readme.update
-util.readme.update: .mk/bin/util-readme-update ## Readme.adoc: Retrieve and insert the latest makefile commands & aliases in the Readme.adoc.
+util.readme.update: .mk/bin/util-readme-update ## Util (Readme.adoc): Retrieve and insert the latest makefile commands & aliases in the Readme.adoc.
 	. .mk/bin/util-readme-update
+
+
+.PHONY: util.php.strict
+util.php.strict: .mk/bin/util-php-strict ## Util (PHP): Insert `<?php declare(strict_types=1);` instead of `<?php` in all PHP files in src/ & tests/ folders.
+	. .mk/bin/util-php-strict

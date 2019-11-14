56293bb4466ec52d06b54a35bf44b9608585359e
jprivet <jprivet@clever-age.com>
2019-11-14 17:53:46 +0100

    dev(NewsRepository.php): create findOnePublishedBySlug()

--- src/Repository/NewsRepository.php
+++ src/Repository/NewsRepository.php
@@ -2,6 +2,21 @@
 
 namespace App\Repository;
 
+use App\Entity\News;
+
 class NewsRepository extends AbstractRepository implements NewsRepositoryInterface
 {
+    /**
+     * {@inheritDoc}
+     */
+    public function findOnePublishedBySlug(string $slug): ?News
+    {
+        return $this->createQueryBuilder('n')
+            ->where('n.slug = :slug')
+            ->andWhere('n.published = :published')
+            ->setParameter('slug', $slug)
+            ->setParameter('published', true)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
 }

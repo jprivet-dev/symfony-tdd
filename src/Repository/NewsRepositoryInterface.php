ac09ff3b03a9ca845792a52a3f025e2ae860c6f9
jprivet <jprivet@clever-age.com>
2019-11-14 17:53:51 +0100

    dev(NewsRepositoryInterface.php): create findOnePublishedBySlug()

--- src/Repository/NewsRepositoryInterface.php
+++ src/Repository/NewsRepositoryInterface.php
@@ -2,6 +2,13 @@
 
 namespace App\Repository;
 
+use App\Entity\News;
+
 interface NewsRepositoryInterface extends RepositoryInterface
 {
+    /**
+     * @param string $slug
+     * @return News|null
+     */
+    public function findOnePublishedBySlug(string $slug): ?News;
 }

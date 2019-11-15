e47534b89258f4c1c2e8d0878e7bab0ef3870c56
jprivet <jprivet@clever-age.com>
2019-11-15 13:22:14 +0100

    dev(10_doctrine.mk): complete commands

--- .mk/10_doctrine.mk
+++ .mk/10_doctrine.mk
@@ -1,1 +1,22 @@
 ## DOCTRINE
+
+PHONY: doctrine.database.create
+doctrine.database.create: ## Doctrine: Create the configured database.
+	$(SYMFONY) doctrine:database:create --if-not-exists
+
+PHONY: doctrine.database.create.force
+doctrine.database.create.force: doctrine.database.drop doctrine.database.create ## Doctrine: Drop & create the configured database.
+
+PHONY: doctrine.database.drop
+doctrine.database.drop: ## Doctrine: Drop the configured database.
+	$(SYMFONY) doctrine:database:drop --if-exists --force
+
+##
+
+PHONY: doctrine.schema.validate
+doctrine.schema.validate: ## Doctrine: Validate the mapping files.
+	$(SYMFONY) doctrine:schema:validate
+
+PHONY: doctrine.mapping.info
+doctrine.mapping.info: ## Doctrine: List mapped entities.
+	$(SYMFONY) doctrine:mapping:info

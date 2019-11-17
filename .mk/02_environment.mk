d52f06a6c5a44d37083bf75e92d40532e2162d9e
jprivet <jprivet@clever-age.com>
2019-11-17 21:22:14 +0100

    dev(02_environment.mk): create env.ready command

--- .mk/02_environment.mk
+++ .mk/02_environment.mk
@@ -10,20 +10,25 @@ env.local.dev: env.local.clean ## Environment: Alias of `env.local.clean`.
 .PHONY: env.local.prod
 env.local.prod: .env.local.prod.dist ## Environment: Copy `.env.local.prod.dist` into `.env.local` (APP_ENV=prod)
 	cp .env.local.prod.dist .env.local
-	$(MAKE_S) cc
-	$(MAKE_S) symfony.about
-	$(MAKE_S) env.app
+	$(MAKE_S) env.ready
 
 .PHONY: env.local.test
 env.local.test: .env.local.test.dist ## Environment: Copy `.env.local.test.dist` into `.env.local` (APP_ENV=test)
 	cp .env.local.test.dist .env.local
-	$(MAKE_S) cc
-	$(MAKE_S) symfony.about
-	$(MAKE_S) env.app
+	$(MAKE_S) env.ready
 
 .PHONY: env.local.clean
 env.local.clean: ## Environment: Remove `.env.local` and use default vars & environment of `.env` (APP_ENV=dev)
 	rm -f .env.local
+	$(MAKE_S) env.ready
+
+
+#
+# INTERNAL
+#
+
+.PHONY: env.ready
+env.ready:
 	$(MAKE_S) cc
 	$(MAKE_S) symfony.about
 	$(MAKE_S) env.app

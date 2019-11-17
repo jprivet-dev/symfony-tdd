67579951a4bb383e8de069c175c2d4b9b3059547
jprivet <jprivet@clever-age.com>
2019-11-17 22:04:21 +0100

    dev(01_project.mk): udpate install commands

--- .mk/01_project.mk
+++ .mk/01_project.mk
@@ -16,13 +16,24 @@ sh: ## Project: app sh access.
 ##
 
 .PHONY: install
-install: env.app docker.start dependencies assets data ready ## Project: Install all (dependencies, data, assets, ...) according to the environment (APP_ENV).
+install: env.app docker.start dependencies assets data ready ## Project: Install all (dependencies, data, assets, ...) according to the current environment (APP_ENV).
+
+.PHONY: install.dev
+install.dev: env.local.dev ## Project: Force the installation for the "dev" environment.
+	$(MAKE_S) install
+
+.PHONY: install.prod
+install.prod: env.local.prod ## Project: Force the installation for the "prod" environment.
+	$(MAKE_S) install
+
+##
 
 .PHONY: dependencies
 dependencies: composer.install yarn.install ## Project: Install the dependencies (only if there have been changes).
 
 .PHONY: assets
-assets: env.app ## Project: Generate all assets according to the environment (APP_ENV).
+assets: ## Project: Generate all assets according to the current environment (APP_ENV).
+	@echo -e "\033[1;43mGenerate assets (APP_ENV=$(APP_ENV))\033[0m"
 ifeq ($(APP_ENV),prod)
 	$(MAKE_S) assets.prod
 else
@@ -44,7 +55,7 @@ fixtures: alice.fixtures.load ## Project: Load all fixtures.
 ##
 
 .PHONY: check
-check: install composer.validate symfony.security.check db.validate tests ## Project: Launch of install / Composer, Security and DB validations / Tests
+check: install.dev composer.validate symfony.security.check db.validate tests ## Project: Launch of install / Composer, Security and DB validations / Tests
 
 .PHONY: tests
 tests: fixtures phpunit ## Project: Launch all tests.
@@ -87,9 +98,8 @@ _build.clean: # Remove 'build' folder.
 ready: symfony.about.light
 	@echo -e "\033[1;42m"
 	@echo -e "READY!"
-	@echo -e "  APP_ENV:    $(APP_ENV) (value in Makefile)"
-	@echo -e
 	@echo -e "  Website:    \e[4m$(URL_WEBSITE)\\033[24m"
 	@echo -e "  API:        \e[4m$(URL_API)\\033[24m"
 	@echo -e "  phpMyAdmin: \e[4m$(URL_PHPMYADMIN)\\033[24m\033[0m"
 	@echo
+	@$(MAKE_S) env.app

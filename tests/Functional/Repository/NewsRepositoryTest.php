7e0869409511ddd915c11964f8580511962d7f8b
jprivet <jprivet@clever-age.com>
2019-11-19 13:51:23 +0100

    dev(NewsRepositoryTest.php): create base of test

---
+++ tests/Functional/Repository/NewsRepositoryTest.php
@@ -0,0 +1,65 @@
+<?php declare(strict_types=1);
+
+namespace App\Tests\Functional\Repository;
+
+use App\Entity\News;
+use App\Repository\NewsRepository;
+use App\Tests\RepositoryTestCase;
+use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
+
+class NewsRepositoryTest extends RepositoryTestCase
+{
+    use RefreshDatabaseTrait;
+
+    const COUNT_ALL = 2;
+    const COUNT_PUBLISHED = 2;
+    const SLUG = 'symfony-live-usa-2018';
+
+    protected function getRepositoryClass()
+    {
+        return NewsRepository::class;
+    }
+
+    public function testFindAll()
+    {
+        // Act
+        $news = $this->repository->findAll();
+
+        // Assert
+        $this->assertCount(self::COUNT_ALL, $news);
+    }
+
+    public function testFindAllPublished()
+    {
+        // Act
+        $news = $this->repository->findAllPublished();
+
+        // Assert
+        $this->assertCount(self::COUNT_PUBLISHED, $news);
+    }
+
+    public function testFindOnePublishedBySlug()
+    {
+        // Arrange
+        $slug = self::SLUG;
+
+        // Act
+        $news = $this->repository->findOnePublishedBySlug($slug);
+
+        // Assert
+        $this->assertInstanceOf(News::class, $news);
+        $this->assertSame($slug, $news->getSlug());
+    }
+
+    public function testFindOnePublishedBySlugWrongSlug()
+    {
+        // Arrange
+        $slug = '_x_x_x_';
+
+        // Act
+        $news = $this->repository->findOnePublishedBySlug($slug);
+
+        // Assert
+        $this->assertNull($news);
+    }
+}

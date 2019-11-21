41f9494c450ff41c51a314e87ee6f6f2f99ffd23
jprivet <jprivet@clever-age.com>
2019-11-21 17:10:23 +0100

    dev(09_xdebug.mk): udpate commands (add @)

--- .mk/09_xdebug.mk
+++ .mk/09_xdebug.mk
@@ -2,10 +2,10 @@
 
 .PHONY: xdebug.on
 xdebug.on: ## Xdebug: Enable the module.
-	$(EXEC_APP_ROOT) sed -i.default "s/^;zend_extension=/zend_extension=/" $(XDEBUG_INI)
+	@$(EXEC_APP_ROOT) sed -i.default "s/^;zend_extension=/zend_extension=/" $(XDEBUG_INI)
 	@echo -e '\033[1;42mXdebug ON\033[0m';
 
 .PHONY: xdebug.off
 xdebug.off: ## Xdebug: Disable the module.
-	$(EXEC_APP_ROOT) sed -i.default "s/^zend_extension=/;zend_extension=/" $(XDEBUG_INI)
+	@$(EXEC_APP_ROOT) sed -i.default "s/^zend_extension=/;zend_extension=/" $(XDEBUG_INI)
 	@echo -e '\033[1;43mXdebug OFF\033[0m';

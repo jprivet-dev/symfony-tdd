4d8ed46f46d1e66c438e986a7f1f8d7ece39dcb7
jprivet <jprivet@clever-age.com>
2019-11-21 16:24:31 +0100

    dev(04_yarn.mk): create yarn.test command

--- .mk/04_yarn.mk
+++ .mk/04_yarn.mk
@@ -9,3 +9,7 @@ yarn.install: ## Yarn: Install all dependencies.
 yarn.upgrade: ## Yarn: Upgrade packages to their latest version based on the specified range.
 	@echo -e "\033[1;43mYarn: Upgrade\033[0m"
 	$(YARN) upgrade
+
+.PHONY: yarn.test
+yarn.test: ## Yarn: Launch test package script (Jest).
+	$(YARN) test
